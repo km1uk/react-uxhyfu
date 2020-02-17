a87908646b3ab49d097a55cec8f01e6bee1035b3
km1uk <keith@kgmm.co.uk>
2020-02-17 18:52:18 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-uxhyfu
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-uxhyfu)
---
+++ components/MyInfo.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+function MyInfo() {
+  return (
+    <div>
+      <h1>name</h1>
+      <p>blurb</p>
+      <ol>
+        <li>a</li>
+        <li>b</li>
+        <li>c</li>
+      </ol>
+    </div>
+  )
+}
+
+export default MyInfo
